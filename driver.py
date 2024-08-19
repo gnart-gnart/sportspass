e8a575c7e05c761e2220f8474e110a30316ee783
Grant <grantwoxford@gmail.com>
2024-08-18 19:30:38 -0500

    Refactoring to enable interacting with hrefs

--- driver.py
+++ driver.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 def waitForElement(driver, by, value, timeout=10):
     return WebDriverWait(driver, timeout).until(
@@ -11,11 +12,18 @@ def findElement(driver, by, value):
     return driver.find_element(by, value)
 
 def interactWithElement(driver, by, value, action='click'):
-    element = waitForElement(driver, by, value)
+    if by == By.XPATH and 'href' in value:
+        element = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((by, f'//a[@href="{value}"]'))
+        )
+    else:
+        element = waitForElement(driver, by, value)
+    
     if action == 'click':
         element.click()
     elif action == 'send_keys':
         return element
     elif action == 'submit':
         element.send_keys(Keys.RETURN)
+    
     return element
